c954c3d7bb66b03c4918a16e4aa3d03538262e7b
NousseibaIOT <nousseiba.gaouche@ump.ac.ma>
2023-01-04 15:16:52 +0100

    fin

--- app/views.py
+++ app/views.py
@@ -16,38 +16,37 @@ def dht113(request):
 #Jour1
 def data24(request):
     #jr1=Dht.objects.filter(temp__range=(32,34)).values().all()[:5]
-    jr1 = Dht.objects.all()[0:75]
+    jr1 = Dht.objects.all()[3:7]
     s1={'jr1':jr1}
     return render(request, 'jour1.html',s1)
 def data24Graphe(request):
-    jr1=Dht.objects.all()[:5]
-    s1={'jr1':jr1}
+    tab=Dht.objects.all()[3:7]
+    s1={'tab':tab}
     return render(request, 'grapheJr1.html',s1)
 #Jour2
 def data48(request):
     #jr1=Dht.objects.filter(temp__range=(32,34)).values().all()[:5]
-    jr1 = Dht.objects.all()[3:72]
-    s1={'jr1':jr1}
-    return render(request, 'jour2.html',s1)
-def data48Graphe(request):
-    jr2=Dht.objects.all()[73:144]
+    jr2 = Dht.objects.all()[7:9]
     s2={'jr2':jr2}
+    return render(request, 'jour2.html',s2)
+def data48Graphe(request):
+    tab=Dht.objects.all()[7:9]
+    s2={'tab':tab}
     return render(request, 'grapheJr2.html',s2)
 #jour3
 def data72(request):
     #jr1=Dht.objects.filter(temp__range=(32,34)).values().all()[:5]
-    jr1 = Dht.objects.filter(temp__range=(32, 34)).values().all()[3:72]
-    s1={'jr1':jr1}
-    return render(request, 'jour3.html',s1)
-def data72Graphe(request):
-    jr3=Dht.objects.all()[145:216]
+    jr3 = Dht.objects.filter(temp__range=(32, 34)).values().all()[3:9]
     s3={'jr3':jr3}
+    return render(request, 'jour3.html',s3)
+def data72Graphe(request):
+    tab=Dht.objects.all()[3:9]
+    s3={'tab':tab}
     return render(request, 'grapheJr3.html',s3)
 #historique
 def historique(request):
     return render(request, 'historique.html')
-def essaie(request):
-    return render(request, 'essaie.html')
+
 
 
 
